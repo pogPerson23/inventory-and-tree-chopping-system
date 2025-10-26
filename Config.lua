002f3573ffe497b0e2de55e00bdf16bdffe5e973
pogPerson23 <poggywoggy23sas@gmail.com>
2025-10-26 17:55:18 +0000

    Create Config.lua

---
+++ Config.lua
@@ -0,0 +1,16 @@
+local config = {
+	hotbarSlotsAmount = 9,
+	
+	equipCooldownTime = 0.15,
+	
+	equippedAnimationName = "equippedAnimation",
+	useAnimationName = "useAnimation",
+	
+	dropButton = Enum.KeyCode.Q,
+	
+	toolModuleName = "ToolModule",
+	
+	maxStackAmount = 64
+}
+
+return config
